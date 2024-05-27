192e3d2431ab4336c630a287b60798cc264095ad
phuch <phuchmth2209055@fpt.edu.vn>
2024-05-27 08:50:03 +0700

    Final Commit

--- src/main/java/com/example/secondspring/ProductRepository.java
+++ src/main/java/com/example/secondspring/ProductRepository.java
@@ -1,2 +1,8 @@
-package com.example.secondspring;public class ProductRepository {
+package com.example.secondspring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//model
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
